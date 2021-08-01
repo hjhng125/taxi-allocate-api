00e7154ee71fbeceff4a76e072227be95c190d71
hjhng125 <hjhng125@nate.com>
2021-08-01 23:36:32 +0900

    initial commit

---
+++ src/main/java/me/hjhng125/taxiallocationapi/TaxiAllocationApiApplication.java
@@ -0,0 +1,13 @@
+package me.hjhng125.taxiallocationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAllocationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiAllocationApiApplication.class, args);
+    }
+
+}
