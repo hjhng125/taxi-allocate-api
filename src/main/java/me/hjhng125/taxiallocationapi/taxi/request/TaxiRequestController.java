a4fd321ec0c8658f1679d3f915e45cebb25084e6
hjhng125 <hjhng125@nate.com>
2021-08-03 01:40:53 +0900

    api 요청 시 member 로그 추가

--- src/main/java/me/hjhng125/taxiallocationapi/taxi/request/TaxiRequestController.java
+++ src/main/java/me/hjhng125/taxiallocationapi/taxi/request/TaxiRequestController.java
@@ -1,6 +1,7 @@
 package me.hjhng125.taxiallocationapi.taxi.request;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.hjhng125.taxiallocationapi.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TaxiRequestController {
@@ -18,6 +20,8 @@ public class TaxiRequestController {
 
     @GetMapping("/taxi-requests")
     public ResponseEntity<Page<TaxiRequestDTO>> list(@AuthenticationPrincipal Member member, @PageableDefault Pageable pageable) {
+        log.info("Member : {}", member);
+
         Page<TaxiRequestDTO> requests = service.requests(member, pageable);
         return ResponseEntity.ok(requests);
     }
